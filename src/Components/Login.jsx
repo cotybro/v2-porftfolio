6bddc5fd15e03150154237227e826ccc414067d3
cotybro <36529826+cotybro@users.noreply.github.com>
2023-04-13 12:10:29 -0500

    Updating header and login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Login.css'
 import CloseIcon from '@mui/icons-material/Close'
+import PersonIcon from '@mui/icons-material/Person'
 import memoji from '../assets/Images/memoji.png'
 
 function Login(props) {
@@ -20,9 +21,12 @@ function Login(props) {
       <div className='login-bottom'>
         <div className='login-col'>
           <CloseIcon className='login-close' />
-          <p>Cancel</p>
+          <p className='close-text'>Cancel</p>
         </div>
-        <div className='login-col'></div>
+        <div className='login-col'>
+          <PersonIcon className='login-person' />
+          <p className='person-text'>Switch User</p>
+        </div>
       </div>
     </div>
   )
